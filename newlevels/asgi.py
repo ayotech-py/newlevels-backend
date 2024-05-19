786a2613c671fa2a2177cf0bd883e9fed040e69f
AutomachoiceSystems <automachoice.systems@gmail.com>
2024-05-19 11:19:23 +0100

    backedn update

--- newlevels/asgi.py
+++ newlevels/asgi.py
@@ -9,9 +9,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
-
-django_asgi_app = get_asgi_application()
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from newlevelsapi.routing import websocket_urlpatterns
@@ -20,6 +17,7 @@ from channels.security.websocket import AllowedHostsOriginValidator, OriginValid
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newlevels.settings')
 
+django_asgi_app = get_asgi_application()
 #application = get_asgi_application()
 
 application = ProtocolTypeRouter({
